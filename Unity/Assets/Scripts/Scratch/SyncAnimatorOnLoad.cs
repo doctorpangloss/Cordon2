37e4ec8bb0e0543e43c196995a61bc1641d48df7
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-09-10 15:05:31 -0400

    Fixing exceptions so that this runs on the phone

---
+++ Unity/Assets/Scripts/Scratch/SyncAnimatorOnLoad.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SyncAnimatorOnLoad : NetworkBehaviour
+{
+	public Animator animator;
+
+	public override bool OnSerialize (NetworkWriter writer, bool initialState)
+	{
+		var serialized = base.OnSerialize (writer, initialState);
+		var time = animator.playbackTime;
+		writer.Write (time);
+		return serialized;
+	}
+
+	public override void OnDeserialize (NetworkReader reader, bool initialState)
+	{
+		base.OnDeserialize (reader, initialState);
+		float time = 0f;
+		try {
+			time = reader.ReadSingle ();
+		} catch (System.Exception e) {
+			Debug.LogError ("Custom deserialization exception.");
+			Debug.LogError (e);
+		}
+		animator.playbackTime = time;
+	}
+}
