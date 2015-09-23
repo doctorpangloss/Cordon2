3b8a85cf0057dfc55fc99c0622a4fdacb60f1026
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-09-23 15:39:37 -0400

    Control network HUD appearance

---
+++ Unity/Assets/Scripts/Scratch/ThreeFingerTap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+namespace Scratch
+{
+	public class ThreeFingerTap : MonoBehaviour
+	{
+		public int fingers = 3;
+		public string tapMessage = "OnFingerTap";
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			if (Input.touchCount == fingers
+				&& Input.touches.Any (t => t.phase == TouchPhase.Began)) {
+				BroadcastMessage (tapMessage, fingers, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/Scratch/ToggleNetworkHUD.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Scratch
+{
+	public class ToggleNetworkHUD : MonoBehaviour
+	{
+		public Behaviour networkManagerHUD;
+		[Header("HUD Starts Enabled")]
+		public bool onMobile = false;
+		public bool others = true;
+
+		void Start ()
+		{
+			if (Application.isMobilePlatform) {
+				networkManagerHUD.enabled = onMobile;
+			} else {
+				networkManagerHUD.enabled = others;
+			}
+		}
+
+		public void Toggle ()
+		{
+			networkManagerHUD.enabled = !networkManagerHUD.enabled;
+		}
+	}
+}
