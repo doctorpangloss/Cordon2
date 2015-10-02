7a0e22ddd735f8041db1aa4d9051696ab26bcb34
changt <tonych@mit.edu>
2015-10-02 12:10:15 -0400

    birds

---
+++ Unity/Assets/Scripts/Scratch/SimpleBirdScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleBirdScript : MonoBehaviour {
+
+    public Vector3 velocity;
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    private Camera mainCamera;
+    private float minLife;
+    private float life;
+    //bird will be automatically removed if life > minLife and not in main Camera
+    //in Seconds
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+        minLife = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += (velocity * Time.deltaTime);
+        life += Time.deltaTime;
+        if (life > minLife && !isInCamera())
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    bool isInCamera()
+    {
+        Vector3 viewPos = mainCamera.WorldToViewportPoint(this.transform.position);
+        return (viewPos.x > 0f && viewPos.x < 1f && viewPos.y > 0f && viewPos.y < 1f && viewPos.z > 0f);
+    }
+}
