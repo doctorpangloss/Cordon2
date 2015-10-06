876565cab2672fd73574c3ddbb4fdf28233e6908
changt <tonych@mit.edu>
2015-10-06 19:05:28 -0400

    sound round 1

---
+++ Unity/Assets/Scripts/Scratch/PlayerMovementSoundEffect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Scratch
+{
+    public class PlayerMovementSoundEffect : MonoBehaviour
+    {
+        public MoveController moveController;
+        public AudioSource footStep;
+        private bool playing = false;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (moveController.moving)
+            {
+                if (!footStep.isPlaying)
+                {
+                    footStep.Play();
+                }
+            } else
+            {
+                if (footStep.isPlaying)
+                {
+                    footStep.Stop();
+                }
+            }
+        }
+    }
+}
